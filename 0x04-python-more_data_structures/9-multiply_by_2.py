be7c90ce5a0ab9c3d5122ecd0c91e14050699f08
awinimaxwell428@gmail.com <awinimaxwell428@gmail.com>
2024-03-06 22:20:46 -0800

    multiply the value by 2 and return the new dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = {}
+    new_dic = {}
     for i in a_dictionary:
-        new_dictionary[i] = a_dictionary[i] * 2
-    return new_dictonary
+        new_dic[i] = a_dictionary[i] * 2
+    return new_dic
