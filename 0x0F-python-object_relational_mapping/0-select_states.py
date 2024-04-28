6671abec08dca664d9c0191569f8a664ffb2a5a5
Awinimaxwelll <awinimaxwell428@gmail.com>
2024-04-28 14:17:46 -0700

    correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-
-"""write a script that list all the states from the database in hbtn_0e_0_usa"""
+"""  Write a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
 import sys
-import mysqldb
 
 
-db = mysqldb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
-cur = db.cursor()
-cur.execute("SELECT * FROM states")
-rw = cur.fetchall()
-for row in rw:
-    print(row)
-cur.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
